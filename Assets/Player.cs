5b152736f051141f3d1d9842c83927ef778af311
narulab <36588522+narulab@users.noreply.github.com>
2021-02-10 23:05:26 +0900

    【Chapter 3】コメントを追加

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,17 +6,23 @@ public class Player : MonoBehaviour
 {
     float x = 0.0f;
 
-    // Update is called once per frame
+    // 毎フレーム実行される
     void Update()
     {
+        // 右矢印が押されている場合
         if (Input.GetKey(KeyCode.RightArrow))
         {
+            // 右に移動
             x += 5.0f * Time.deltaTime;
         }
+        // 左矢印が押されている場合
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            // 左に移動
             x -= 5.0f * Time.deltaTime;
         }
+
+        // 位置を更新
         transform.localPosition = new Vector3(x, 0, 0);
     }
 }
