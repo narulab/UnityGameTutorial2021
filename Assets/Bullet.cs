a1197c6fc71302e52ec3fa78a2dcce86299a952e
narulab <36588522+narulab@users.noreply.github.com>
2021-02-20 23:16:21 +0900

    【Chapter 5】Bulletが下に移動するように

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    float y = 12.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // 下に移動
+        y -= 10.0f * Time.deltaTime;
+
+        // 位置を更新
+        transform.localPosition = new Vector3(0, y, 0);
     }
 }
